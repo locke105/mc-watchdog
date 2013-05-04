291df801d1ca4ee843bca7b38e0b1725fcce8596
mrodden <locke105@gmail.com>
2013-05-03 22:49:01 -0500

    Update license to Apache2
    Add LICENSE and put header on watchdog

--- watchdog.py
+++ watchdog.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-# author: Mathew Odden <locke105@gmail.com>
-# license: Apache 2.0
+# Copyright 2013 Mathew Odden <locke105@gmail.com>
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
 
 import logging
 import shlex
@@ -95,7 +107,7 @@ class Service(object):
             LOG.warning("Couldn't get server info!")
             return False
 
-        LOG.info("Server info: %s" % sinfo)
+        LOG.debug("Server info: %s" % sinfo)
         return True
 
 
